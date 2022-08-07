79ed1650be13c4017741dfb07d811d6cc5a1a049
Rishika Vishnoi <Rishikavishnoi56@gmail.com>
2022-08-07 23:30:24 +0530

    adding all folder

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Box from './Box';
+import './style.css';
+import Login from './Login';
+
+function App(){
+  return(
+ <div>
+  <h1 className='heading'>Weather App </h1>
+  <Box >
+ <Login /> </Box>
+
+  
+ </div>
+  )
+}
+export default App;
